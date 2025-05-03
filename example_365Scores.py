f11255323155e21a1fa59528784565399d7c336d
Dxx <malaksanchez09@gmail.com>
2025-05-03 14:52:54 -0500

    new example

--- example_365Scores.py
+++ example_365Scores.py
@@ -1,11 +1,17 @@
 import footyIG as fty
 
 scores = fty.Scores365()
-today = scores.get_league_top_players_stats("Betplay Dimayor")
-print(today.head())
+'''scorers = scores.get_league_top_scorers('Libertadores')
+print(scorers.head())
 
 cityVSvilla = "https://www.365scores.com/es-mx/football/match/premier-league-7/aston-villa-manchester-city-109-110-7#id=4147355"
 game = scores.extract_statistics(cityVSvilla)
-print(game.head())
+print(game.head())'''
+
+leipzigVSbayern = 'https://www.365scores.com/es-mx/football/match/bundesliga-25/bayern-munich-rb-leipzig-331-7171-25#id=4168062'
+stats = scores.get_players_stats(leipzigVSbayern)
+print(stats.head())
+
+
 
 
