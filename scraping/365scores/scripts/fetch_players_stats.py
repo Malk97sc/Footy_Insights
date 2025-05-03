837ad9c34d9157702850ae8e4b7188f45747862e
Dxx <malaksanchez09@gmail.com>
2025-05-03 12:06:41 -0500

    testing new functions

--- scraping/365scores/scripts/fetch_players_stats.py
+++ scraping/365scores/scripts/fetch_players_stats.py
@@ -3,8 +3,16 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-from scraper import get_league_top_players_stats, get_today_games
+from scraper import *
 
-df = get_league_top_players_stats(league='Premier League')
+'''df = get_league_top_players_stats(league='Premier League')
 df = get_today_games('Premier League')
+print(df.head())
+
+url1 = 'https://www.365scores.com/es-mx/football/match/bundesliga-25/bayern-munich-rb-leipzig-331-7171-25#id=4168062'
+df2 = get_players(url1, True)
+print(df2.head())'''
+
+url = 'https://www.365scores.com/es-mx/football/match/bundesliga-25/bayern-munich-rb-leipzig-331-7171-25#id=4168062'
+df = get_players_stats(url, True)
 print(df.head())
